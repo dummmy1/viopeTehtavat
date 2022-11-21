47d3ac8a115b20bc57165d32ea09beb2b4c46321
dummy <jarkko.jlaurila@gmail.com>
2022-11-21 09:31:56 +0200

    tehtävät 1 viikko 10

---
+++ viope10/viope10 t1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+//#include <>
+
+
+using namespace std;
+
+struct Opiskelija {
+    string etuNimi;
+    string sukuNimi;
+    int opintoPisteet;
+    int opiskelijanumero;
+};
+
+
+
+int main(){
+    Opiskelija opiskelija1;
+    cout << "Anna opiskelijan etunimi: ";
+    cin >> opiskelija1.etuNimi;
+    cout << "Anna opiskelijan sukunimi: ";
+    cin >> opiskelija1.sukuNimi;
+    cout << "Anna opiskelijan opiskelijanumero: ";
+    cin >> opiskelija1.opiskelijanumero;
+    cout << "Anna opisjelijan opintopisteet: ";
+    cin >> opiskelija1.opintoPisteet;
+
+    cout << "Etunimi: "<<opiskelija1.etuNimi << "\n";
+    cout << "Sukunimi: "<<opiskelija1.sukuNimi << "\n";
+    cout << "Opiskelijanumero: "<<opiskelija1.opiskelijanumero << "\n";
+    cout << "Opintopisteet: "<<opiskelija1.opintoPisteet << "\n";
+
+}
